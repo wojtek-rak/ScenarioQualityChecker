1289a76093261e6554c4fe0b74cf0e7c700db579
wolix07 <rak.wojtek@wp.pl>
2019-12-24 00:20:55 +0100

    Add mock and test

--- src/main/java/pl/put/poznan/transformer/logic/operations/ScenarioWithNumbering.java
+++ src/main/java/pl/put/poznan/transformer/logic/operations/ScenarioWithNumbering.java
@@ -4,15 +4,22 @@ import pl.put.poznan.transformer.logic.abstraction.ScenarioItemVisitor;
 import pl.put.poznan.transformer.logic.abstraction.ScenarioStep;
 import pl.put.poznan.transformer.logic.models.scenarios.Scenario;
 import pl.put.poznan.transformer.logic.models.scenarios.ScenarioRoot;
+import pl.put.poznan.transformer.logic.models.scenarios.StringBuilderWrapper;
 import pl.put.poznan.transformer.rest.models.CountScenarioItemsResponse;
 import pl.put.poznan.transformer.rest.models.ScenarioWithNumberingResponse;
 
 public class ScenarioWithNumbering extends ScenarioItemVisitor<ScenarioWithNumberingResponse> {
-    private StringBuilder stringBuilder = new StringBuilder();
+
+    public ScenarioWithNumbering(StringBuilderWrapper sb)
+    {
+        stringBuilderWrapper = sb;
+    }
+
+    private StringBuilderWrapper stringBuilderWrapper;
 
     protected void visit(String prefix, int counter, Scenario scenario) {
         for (ScenarioStep step : scenario) {
-            stringBuilder.append(prefix)
+            stringBuilderWrapper.append(prefix)
                     .append(counter).append(". ")
                     .append(step.getScenarioHeader()).append("\n"); ;
 
@@ -27,16 +34,16 @@ public class ScenarioWithNumbering extends ScenarioItemVisitor<ScenarioWithNumbe
     @Override
     protected ScenarioWithNumberingResponse getResult() {
         ScenarioWithNumberingResponse result = new ScenarioWithNumberingResponse();
-        result.scenario = stringBuilder.toString();
+        result.scenario = stringBuilderWrapper.toString();
         return result;
     }
 
     @Override
     public ScenarioWithNumberingResponse execute(){
         ScenarioRoot scenarioRoot = (ScenarioRoot)getScenario();
-        stringBuilder.append("Tytuł: ").append(scenarioRoot.title).append("\n");
-        stringBuilder.append("Aktorzy: ").append(scenarioRoot.actors).append("\n");
-        stringBuilder.append("System: ").append(scenarioRoot.systemActor).append("\n");
+        stringBuilderWrapper.append("Tytuł: ").append(scenarioRoot.title).append("\n");
+        stringBuilderWrapper.append("Aktorzy: ").append(scenarioRoot.actors).append("\n");
+        stringBuilderWrapper.append("System: ").append(scenarioRoot.systemActor).append("\n");
         visit("", 1, scenarioRoot.scenario);
         return getResult();
     }
