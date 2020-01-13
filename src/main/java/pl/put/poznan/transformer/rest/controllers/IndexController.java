97a15a293c84b24c06803f79875b3a300c66b892
mareksubocz <marsub@wp.pl>
2020-01-13 11:29:56 +0100

    First html page

---
+++ src/main/java/pl/put/poznan/transformer/rest/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package pl.put.poznan.transformer.rest.controllers;
+
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
