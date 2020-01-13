07acebc444295ffbac64addac42507823b779626
mareksubocz <marsub@wp.pl>
2020-01-13 13:48:33 +0100

    ui działa

--- src/main/java/pl/put/poznan/transformer/rest/controllers/IndexController.java
+++ src/main/java/pl/put/poznan/transformer/rest/controllers/IndexController.java
@@ -1,12 +1,15 @@
 package pl.put.poznan.transformer.rest.controllers;
 
         import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
         import org.springframework.web.bind.annotation.RequestMapping;
+        import pl.put.poznan.transformer.logic.managers.DataManager;
 
 @Controller
 public class IndexController {
     @RequestMapping("/index")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("ids_set", DataManager.getInstance().scenarioCollection.keySet());
         return "index";
     }
 }
